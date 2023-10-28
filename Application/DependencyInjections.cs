0b53a563b93138194268ed3d8a54f231850359f1
Bình Nguyen Thanh <binhnguyenthanh19242yahoo@gmail.com>
2023-10-28 16:32:59 +0700

    New Implementation

--- Application/DependencyInjections.cs
+++ Application/DependencyInjections.cs
@@ -9,21 +9,21 @@ public static class DependencyInjections
 {
     public static IServiceCollection AddRepositoryDIs(this IServiceCollection services)
     {
-        services.AddScoped<IAccountRepository, AccountRepository>();
-        services.AddScoped<INewsRepository, NewsRepository>();
-        services.AddScoped<ISpeciesRepository, SpeciesRepository>();
-        services.AddScoped<IAnimalRepository, AnimalRepsository>();
-        services.AddScoped<IAreaRepository, AreaRepository>();
-        services.AddScoped<ICageRepository, CageRepository>();
-        services.AddScoped<IDietRepository, DietRepository>();
-        services.AddScoped<ITicketRepository, TicketRepository>();
-        services.AddScoped<ITicketOrderRepository, TicketOrderRepository>();
-        //IEnumerable<Type> enumerable = Assembly.GetExecutingAssembly().GetTypes().Where(x => x.Name.EndsWith("Repository") && x.IsClass);
-        //foreach (var item in enumerable)
-        //{
-        //    var @interface= item.GetInterface("I" + item.Name);
-        //    services.AddScoped(@interface,item);
-        //}
+        //services.AddScoped<IAccountRepository, AccountRepository>();
+        //services.AddScoped<INewsRepository, NewsRepository>();
+        //services.AddScoped<ISpeciesRepository, SpeciesRepository>();
+        //services.AddScoped<IAnimalRepository, AnimalRepsository>();
+        //services.AddScoped<IAreaRepository, AreaRepository>();
+        //services.AddScoped<ICageRepository, CageRepository>();
+        //services.AddScoped<IDietRepository, DietRepository>();
+        //services.AddScoped<ITicketRepository, TicketRepository>();
+        //services.AddScoped<ITicketOrderRepository, TicketOrderRepository>();
+        IEnumerable<Type> enumerable = Assembly.GetExecutingAssembly().GetTypes().Where(x => x.Name.EndsWith("Repository") && x.IsClass);
+        foreach (var item in enumerable)
+        {
+            var @interface = item.GetInterface("I" + item.Name);
+            services.AddScoped(@interface, item);
+        }
         return services;
     }
 }
